7b0b020c8146abdfa6cbc6dc49dcf0c5e5c1d4ba
Marcy Villegas <marcyvillegas17@gmail.com>
2022-11-07 21:29:37 +0800

    added hamburger menu icon

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,64 @@ function App() {
     projects: false,
     contacts: false
   });
+  const [activeMenuIcon, setActiveMenuIcon] = useState(false);
 
   // FUNCTIONS
   const changeActiveTab = (tab) => {
 
-    setActiveTabs({
+    if (tab == "home" && activeTabs.home) return
 
+    setActiveTabs({
+      home: false,
+      [tab]: true
     });
   }
 
   return (
-    <div className="App">
+    <div>
 
-      <div className="flex justify-center gap-10 my-14">
+      {/* Navbar for mobile view */}
+      <div className="flex lg:hidden flex-col">
+
+        <div className="flex justify-end mt-10 mr-10">
+
+          <motion.button
+            className="bg-[#3B3B48] p-2 rounded-md"
+            whileTap={{ scale: 0.9 }}
+            onClick={() => setActiveMenuIcon(!activeMenuIcon)}>
+            <img src="/src/assets/menu-icon.svg" />
+          </motion.button>
+        </div>
+
+        <div className="flex justify-end mr-10">
+          <div className="relative mt-2">
+            {activeMenuIcon &&
+              <ul className="absolute bg-white right-0">
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Contacts</li>
+              </ul>}
+          </div>
+        </div>
+      </div>
+
+      {/* Navigation tabs for desktop view */}
+      <div className="hidden lg:flex justify-center gap-10 mt-14">
         <motion.button
           className={`${activeTabs.home ? "bg-[#5C0099]" : "bg-[#262633]"} p-3 rounded-md text-white font-bold font-inter`}
-          whileTap={{ y: 5 }}>
+          whileTap={{ y: 5 }}
+          onClick={() => changeActiveTab("home")}>
           Home</motion.button>
 
         <motion.button
           className={`${activeTabs.projects ? "bg-[#5C0099]" : "bg-[#262633]"} p-3 rounded-md text-white font-bold font-inter`}
-          whileTap={{ y: 5 }}>
+          whileTap={{ y: 5 }}
+          onClick={() => changeActiveTab("projects")}>
           Projects</motion.button>
 
         <motion.button className={`${activeTabs.contacts ? "bg-[#5C0099]" : "bg-[#262633]"} p-3 rounded-md text-white font-bold font-inter`}
-          whileTap={{ y: 5 }}>
+          whileTap={{ y: 5 }}
+          onClick={() => changeActiveTab("contacts")}>
           Contacts</motion.button>
       </div>
 
